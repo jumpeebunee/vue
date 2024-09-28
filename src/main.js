2848b7fa1daa27578b84d369bfd027dc565d6fad
jumpeebunee <jumpeebunee@gmail.com>
2024-09-28 23:17:54 +0300

    Creating an App

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import Button from "./components/Button.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+const secondApp = createApp(App);
+
+// Captures errors from all descendant component
+app.config.errorHandler = (err) => {
+  console.log(err);
+};
+
+// Available for use anywhere in app
+app.component("Button", Button);
+
+// Multiple application instances
+app.mount("#app");
+secondApp.mount("#app2");
