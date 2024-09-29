cab3e4f65f8d9374dc527a94f605dcef99f612a8
jumpeebunee <jumpeebunee@gmail.com>
2024-09-29 14:53:00 +0300

    deep reactivity and tick

--- src/Apps/App.vue
+++ src/Apps/App.vue
@@ -1,4 +1,6 @@
 <script>
+import { nextTick } from "vue";
+
 export default {
   data() {
     return {
@@ -8,14 +10,23 @@ export default {
       nextButtonAttr: {
         disabled: false,
       },
+      stats: {
+        clickedTimes: 0,
+      },
     };
   },
   methods: {
     clickPrev() {
       this.filmId -= 1;
+      this.updateClickedTimes();
     },
     clickNext() {
       this.filmId += 1;
+      this.updateClickedTimes();
+    },
+    async updateClickedTimes() {
+      this.stats.clickedTimes += 1;
+      await nextTick();
     },
   },
 };
@@ -23,6 +34,8 @@ export default {
 
 <template>
   <h1>Film ID: {{ filmId }}</h1>
+  <div class="times">Total Clicked: {{ stats.clickedTimes }}</div>
+
   <div v-html="banner"></div>
 
   <div class="buttons">
@@ -32,6 +45,10 @@ export default {
 </template>
 
 <style>
+.times {
+  margin-bottom: 3px;
+}
+
 .buttons {
   display: flex;
   margin-top: 12px;
