affe8440af603ff1b7e91295b96e92c4751677ff
jumpeebunee <jumpeebunee@gmail.com>
2024-10-12 13:46:21 +0300

    Lifecycle Hooks

---
+++ src/components/LifecycleHooks/LifecycleHooks.vue
@@ -0,0 +1,33 @@
+<template>
+  {{ message }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Not created",
+    };
+  },
+  beforeCreate() {
+    this.message = "Before created";
+  },
+  created() {
+    this.message = "Created";
+  },
+  beforeMount() {
+    this.message = "Before mounted";
+  },
+  mounted() {
+    this.message = "Mounted";
+  },
+  unmounted() {
+    this.message = "Destroy";
+  },
+  updated() {
+    this.message = "Updated";
+  },
+};
+</script>
+
+<style></style>
