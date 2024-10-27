dbaab6088487bd32372f5b64218b87d12f18106d
jumpeebunee <jumpeebunee@gmail.com>
2024-10-27 12:12:41 +0300

    Comonents Basic - using

---
+++ src/components/ComponentsBasic/ComponentsBasic.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="content">
+    <ButtonCounter />
+    <ButtonCounter />
+  </div>
+</template>
+
+<script>
+import ButtonCounter from "./components/ButtonCounter.vue";
+
+export default {
+  components: {
+    ButtonCounter,
+  },
+};
+</script>
+
+<style>
+.content {
+  display: flex;
+  gap: 16px;
+}
+</style>
---
+++ src/components/ComponentsBasic/components/ButtonCounter.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<template>
+  <button @click="count++" class="btn">Button clicked: {{ count }}</button>
+</template>
+
+<style>
+.btn {
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+  background: #005bff;
+  border: none;
+  padding: 15px 30px;
+  border-radius: 20px;
+  cursor: pointer;
+}
+</style>
